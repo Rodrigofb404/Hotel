daad0ae01e73e8a47fedbdd91bf643533bd4d790
rodrigo <rodrigof0501@gmail.com>
2024-09-10 17:52:32 -0300

    Add properties

--- reserva.py
+++ reserva.py
@@ -2,24 +2,44 @@ from quarto import Quarto
 from cliente import Cliente
 
 class Reserva():
-    def __init__(self) -> None:
-        self.__data_entrada = None
-        self.__data_saida = None
-        self.__quarto = None
-        self.__cliente = None
-        self.__status = None
-        self.__n_reserva = None
-
-    @property
-    def status(self):
-        return self.__status
-
-    def criar_reserva(self, data_entrada : str, data_saida : str, quarto: Quarto, cliente : Cliente, n_reserva: int) -> None:
+    def __init__(self, data_entrada : str, data_saida : str, quarto: Quarto, cliente : Cliente, n_reserva : int) -> None:
         self.__data_entrada = data_entrada
         self.__data_saida = data_saida
         self.__quarto = quarto
         self.__cliente = cliente
-        self.__status = "Reservado"
         self.__n_reserva = n_reserva
+        
+
+    @property
+    def data_entrada(self):
+        return self.__data_entrada
+
+    @property
+    def data_saida(self):
+        return self.__data_saida
+
+    @property
+    def quarto(self):
+        return self.__quarto
+        
+    @property
+    def cliente(self):
+        return self.__cliente
+
+    @property
+    def n_reserva(self):
+        return self.__n_reserva
+
+    @n_reserva.setter
+    def set_n_reserva(self, n_reserva):
+        self.__n_reserva = n_reserva
+
+    # def criar_reserva(self, data_entrada : str, data_saida : str, quarto: Quarto, cliente : Cliente, n_reserva: int) -> None:
+    #     self.__data_entrada = data_entrada
+    #     self.__data_saida = data_saida
+    #     self.__quarto = quarto
+    #     self.__cliente = cliente
+    #     self.__status = "Reservado"
+    #     self.__n_reserva = n_reserva
 
         
